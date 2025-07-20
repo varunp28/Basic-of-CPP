1fa91cf153b75d9954fb9fdeecb1bcde9009dc9e
varunp28 <varunparandkar@gmail.com>
2025-07-20 21:57:24 +0530

    Create Basic Calculator.cpp

---
+++ Basic Calculator.cpp
@@ -0,0 +1,17 @@
+int main(){
+    int sum, difference,product;
+    float num1,num2,div;
+    cout<< "Enter number 1:";
+    cin>> num1;
+    cout<< "Enter number 2:";
+    cin>> num2;
+    sum=num1+num2;
+    cout<<"sum is: " <<sum << endl;
+    difference=num1-num2;
+    cout<<"difference is :"<< difference <<endl;
+    product=num1*num2;
+    cout<<"product is:"<< product<< endl;
+    div=num1/num2;
+    cout<<"the Quotient is :"<<div << endl;
+    return 0;
+}
